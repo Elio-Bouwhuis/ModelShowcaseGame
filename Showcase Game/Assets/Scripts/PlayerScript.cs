7f75e47cdb61ac8bedb6605b84c6e7d40a469478
Elio Bouwhuis <50573900+Elio-Bouwhuis@users.noreply.github.com>
2023-07-08 15:26:56 +0200

    Imported model, made animations work, added hitbox, changed resolution

--- Showcase Game/Assets/Scripts/PlayerScript.cs
+++ Showcase Game/Assets/Scripts/PlayerScript.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
+    private Animation anim;
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = GameObject.Find("Player").GetComponent<Animation>();
     }
 
     // Update is called once per frame
@@ -16,11 +17,18 @@ public class PlayerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) 
         {
-            Debug.Log("Jump");
+            anim.Play("Armature|Jumping");
+            anim["Armature|Jumping"].speed = 2.0f;
         }
         else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Slide");
+            anim.Play("Armature|Sliding2");
+            anim["Armature|Sliding2"].speed = 1.5f;
+        }
+
+        if (anim["Armature|Jumping"].time > 2.49f || anim["Armature|Sliding2"].time > 2.49f)
+        {
+            anim.Play("Armature|Running");
         }
     }
 
